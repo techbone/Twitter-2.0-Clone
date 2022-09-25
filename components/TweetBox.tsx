3a70478fbd80aec0d3dd3e00a281ee87eee034b7
Musa Muhammad <musaawwaletudaye@gmail.com>
2022-09-25 11:48:04 +0100

    feat: adding functionality to the text box

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import {
   CalendarIcon,
@@ -10,8 +10,9 @@ import {
 } from "@heroicons/react/outline";
 
 const TweetBox = () => {
+  const [input, setInput] = useState<string>("");
   return (
-    <div className="flex">
+    <div className="flex space-x-2 p-5">
       <img
         src="https://links.papareact.com/gll"
         alt="image"
@@ -20,7 +21,9 @@ const TweetBox = () => {
       <div className=" flex flex-1 items-center pl-2">
         <form className="flex flex-1 flex-col">
           <input
-            className="outline-none"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="outline-none w-full text-xl h-24 placeholder:text-xl"
             type="text"
             placeholder="What's Happening?"
           />
@@ -29,13 +32,16 @@ const TweetBox = () => {
               className="flex space-x-2 text-twitter flex-1
           "
             >
-              <PhotographIcon className="h-5 w-5" />
+              <PhotographIcon className="h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150" />
               <ServerIcon className="h-5 w-5" />
               <EmojiHappyIcon className="h-5 w-5" />
               <CalendarIcon className="h-5 w-5" />
               <LocationMarkerIcon className="h-5 w-5" />
             </div>
-            <button className="bg-twitter px-5 py-2 text-white rounded-full">
+            <button
+              disabled={!input}
+              className="bg-twitter px-5 py-2 text-white disabled:opacity-40 rounded-full"
+            >
               Tweet
             </button>
           </div>
